2563d87ed5ffb7040471b76709db69769fa0efa9
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-09-25 17:21:27 +0200

    refactoring

--- include/display.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <stdbool.h>
-
-#define PWIDTH 64
-#define PHEIGHT 32
-typedef struct{
-    bool grid[PWIDTH][PHEIGHT];
-}display;
--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "chip8.h"
-#include "display.h"
-
-int main(int argc,int**argv){
-
-}
