42a2a5dda6c1f489022eae01d71943731e13db55
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-09-25 23:46:17 +0200

    Added keys to the chip8 struct ,fetch function

--- src/include/chip8.h
+++ src/include/chip8.h
@@ -2,6 +2,7 @@
 #define CHIP8_H
 
 #include <stdint.h>
+#include "display.h"
 
 #define MEM_SIZE 4096
 #define REGISTER_COUNT 16
@@ -19,12 +20,16 @@ typedef struct
     uint8_t stimer;
     uint8_t V[REGISTER_COUNT];
 
+    bool keys[16];
 } chip8;
 
 extern const uint8_t font[80];
 
 void chip8_init(chip8 *cpu);
 
-void chip8_exec(chip8 *cpu,uint16_t opcode);
+void chip8_cycle(chip8 *cpu,display *disp);
+
+void chip8_exec(chip8 *cpu,display *disp,uint16_t opcode);
+
 
 #endif
