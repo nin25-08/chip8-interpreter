7b1b7e834010c9e2e26be9106548ac0bda8bf028
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-09-25 23:26:05 +0200

    Opcodes 0x5 to 0xFX29, fixed bugs relative to program counter not skipping 2 instructions on skip  opcodes plus missing VF register update on 0x8000 block operations

--- src/chip8.c
+++ src/chip8.c
@@ -75,7 +75,7 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
         // returning from a subroutine is done with 00EE
         if (opcode == 0x00EE)
         {
-            cpu->pc = chip8_pop(cpu);
+            cpu->pc = chip8_pop(cpu) + 2;
         }
 
         break;
@@ -95,7 +95,11 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
     case 0x3000:
         if (cpu->V[x] == NN)
         {
-            cpu->pc++;
+            cpu->pc += 4;
+        }
+        else
+        {
+            cpu->pc += 2;
         }
         break;
 
@@ -103,7 +107,11 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
     case 0x4000:
         if (cpu->V[x] != NN)
         {
-            cpu->pc++;
+            cpu->pc += 4;
+        }
+        else
+        {
+            cpu->pc += 2;
         }
         break;
 
@@ -111,7 +119,11 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
     case 0x5000:
         if (cpu->V[x] == cpu->V[y])
         {
-            cpu->pc + 2;
+            cpu->pc += 4;
+        }
+        else
+        {
+            cpu->pc += 2;
         }
         break;
 
@@ -153,32 +165,48 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
             cpu->pc += 2;
             break;
             // 8XY4 VX is set to the value of VX plus the value of VY. VY is not affected.
-        case 0x4:
-            cpu->V[x] = cpu->V[x] + cpu->V[y];
+        case 0x4: {
+            uint16_t sum = cpu->V[x] + cpu->V[y];
+            cpu->V[0xF] = (sum > 255) ? 1 : 0;
+            cpu->V[x] = sum & 0xFF;
             cpu->pc += 2;
             break;
-            // 8XY5 sets VX to the result of VX - VY.
-        case 0x5:
+        }
+        case 0x5: {
+            cpu->V[0xF] = (cpu->V[x] >= cpu->V[y]) ? 1 : 0;
             cpu->V[x] = cpu->V[x] - cpu->V[y];
             cpu->pc += 2;
             break;
-            // 8XY6 put the value of VY into VX, and then shifted the value in VX 1 bit to the right
-        case 0x6:
+        }
+        case 0x6: {
+            cpu->V[0xF] = cpu->V[y] & 0x1;
             cpu->V[x] = cpu->V[y] >> 1;
             cpu->pc += 2;
             break;
-            // 8XY7 sets VX to the result of VY - VX.
+        }
+        case 0x7: {
+            cpu->V[0xF] = (cpu->V[y] >= cpu->V[x]) ? 1 : 0;
             cpu->V[x] = cpu->V[y] - cpu->V[x];
-        case 0x7:
             cpu->pc += 2;
             break;
-            // 8XY6 put the value of VY into VX, and then shifted the value in VX 1 bit to the left
-        case 0xE:
+        }
+        case 0xE: {
+            cpu->V[0xF] = (cpu->V[y] >> 7) & 0x1;
             cpu->V[x] = cpu->V[y] << 1;
             cpu->pc += 2;
             break;
         }
 
+        break;
+        case 0x9000:
+        if (cpu->V[x] != cpu->V[y])
+        {
+            cpu->pc += 4;
+        }
+        else
+        {
+            cpu->pc += 2;
+        }
         break;
         // 0XANNN This sets the index register I to the value NNN.
     case 0xA000:
@@ -191,11 +219,132 @@ void chip8_exec(chip8 *cpu, display *disp, uint16_t opcode)
         break;
         // 0xCXNN This instruction generates a random number, binary ANDs it with the value NN, and puts the result in VX.
     case 0xC000:
-      cpu->V[x] = (rand() % 256) & NN;
-    break;
+        cpu->V[x] = (rand() % 256) & NN;
+        cpu->pc += 2;
         break;
+        /*0xDXYZ It will draw an N pixels tall sprite from the memory location
+         that the I index register is holding to the screen, at the horizontal X coordinate in VX
+         and the Y coordinate in VY*/
+    case 0xD000:;
+        uint8_t x_start = cpu->V[x] % 64;
+        uint8_t y_start = cpu->V[y] % 32;
+        uint8_t height = z;
 
-    case 0xD000: // display / draw
+        cpu->V[0xF] = 0x0;
+
+        for (uint8_t row = 0; row < height; row++)
+        {
+            uint8_t py = y_start + row;
+            if (py >= 32)
+                break;
+
+            uint8_t sprite_data = cpu->ram[cpu->I + row];
+            uint8_t mask = 0x80;
+
+            for (int sig = 0; sig < 8; sig++)
+            {
+                uint8_t px = x_start + sig;
+                if (px >= 64)
+                    break;
+
+                bool toEval = sprite_data & mask;
+                if (toEval)
+                {
+                    if (disp->pixels[py][px])
+                    {
+                        cpu->V[0xF] = 1;
+                    }
+                    disp->pixels[py][px] ^= true;
+                }
+                mask >>= 1;
+            }
+        }
+        cpu->pc += 2;
+        break;
+        // skip if key
+    case 0xE000:
+        switch (NN)
+        {
+            // EX9E will skip one instruction (increment PC by 2)
+            // if the key corresponding to the value in VX is pressed.
+        case 0x9E:
+            if (cpu->keys[cpu->V[x]])
+            {
+                cpu->pc += 4;
+            }
+            else
+            {
+                cpu->pc += 2;
+            }
+            break;
+            // EXA1 skips if the key corresponding to the value in VX is not pressed.
+        case 0xA1:
+            if (!cpu->keys[cpu->V[x]])
+            {
+                cpu->pc += 4;
+            }
+            else
+            {
+                cpu->pc += 2;
+            }
+            break;
+        }
+        break;
+    case 0xF000:
+        switch (NN)
+        {
+            // FX07 sets VX to the current value of the delay timer
+        case 0x07:
+            cpu->V[x] = cpu->dtimer;
+            cpu->pc += 2;
+            break;
+            // FX15 sets the delay timer to the value in VX
+        case 0x15:
+            cpu->dtimer = cpu->V[x];
+            cpu->pc += 2;
+            break;
+            // FX18 sets the sound timer to the value in VX
+        case 0x18:
+            cpu->stimer = cpu->V[x];
+            cpu->pc += 2;
+            break;
+            // FX1E The index register I will get the value in VX added to it.
+        case 0x1E:
+            cpu->I += cpu->V[x];
+            cpu->pc += 2;
+            break;
+            // FX0A: Get key This instruction “blocks”; it stops executing instructions
+            // and waits for key input (or loops forever, unless a key is pressed).
+        case 0x0A:
+            bool isPressed = false;
+            for (int i = 0; i < 16; i++)
+            {
+                if (cpu->keys[i])
+                {
+                    isPressed = true;
+                    cpu->V[x] = i;
+                    break;
+                }
+            }
+
+            if (isPressed)
+            {
+                cpu->pc += 2;
+            }
+            break;
+
+        case 0x29:
+            uint8_t character = cpu->V[x] & 0x0F;
+            cpu->I = character * 5 + FONT_SET_START_ADDRESS;
+            cpu->pc += 2;
+            break;
+        case 0x33:
+            break;
+        case 0x55:
+            break;
+        case 0x65:
+            break;
+        }
         break;
     }
 }
