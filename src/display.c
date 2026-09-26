de7302e8134b32a46ff241cc8c2ac982520f0b32
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-09-26 17:09:45 +0200

    Basic display funcitons through SDL2 graphic library

---
+++ src/display.c
@@ -0,0 +1,61 @@
+#include "display.h"
+
+static SDL_Window *window = NULL;
+static SDL_Renderer *renderer = NULL;
+
+static void display_draw_pixel(int x, int y, bool flag)
+{
+    if (flag)
+    {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+    else
+    {
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        SDL_RenderDrawPoint(renderer, x, y);
+    }
+}
+bool display_init()
+{
+    if (SDL_Init(SDL_INIT_VIDEO) != 0)
+    {
+        return false;
+    }
+    if (SDL_CreateWindowAndRenderer(64 * 4, 32 * 4, 0, &window, &renderer) != 0)
+    {
+        return false;
+    }
+
+    SDL_RenderSetLogicalSize(renderer, 64, 32);
+    // floods the blackground with back
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
+    SDL_RenderClear(renderer);
+
+    return true;
+}
+
+
+
+void display_draw(display *disp){
+    for(int i=0;i<PWIDTH;i++){
+         for(int j=0;i<PHEIGHT;j++){
+            display_draw_pixel(i,j,disp->pixels[i][j]);
+    }
+}
+}
+
+void display_clear()
+{
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 100);
+    SDL_RenderClear(renderer);
+}
+
+void display_update()
+{
+    SDL_RenderPresent(renderer);
+}
+
+void display_match(){
+
+}
