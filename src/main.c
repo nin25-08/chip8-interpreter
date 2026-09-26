5accda3487f9c554c7498c44bc213a2bd9066939
nin25-08 <315899434+nin25-08@users.noreply.github.com>
2026-09-26 16:47:40 +0200

    load rom function

--- src/main.c
+++ src/main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 #include "chip8.h"
 #include "display.h"
 
+void loadRom(chip8 *cpu,const char* filedir){
+    FILE *fp = fopen(filedir,"r");
+    if(fp==NULL){
+        printf("cannot open fp!");
+        return;
+    }
+    fseek(fp,0,SEEK_END);
+    long size = ftell(fp);
+    if(size>MEM_SIZE-MEM_RESERVED){
+        printf("fp too big!");
+        fclose(fp);
+        return;
+    }
+    rewind(fp);
+
+    fread(&cpu->ram[0x200],1,size,fp);
+    fclose(fp);
+}
 int main(int argc,int**argv){
-    while(1)
+
 }
